f1867d870b91bc1b35b4f84858f8f9c437551da2
tobiayeni <mgu648@usask.ca>
2025-02-03 15:13:12 -0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Tobi Ayeni on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testTests/testTests.swift
@@ -0,0 +1,16 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Tobi Ayeni on 2025-02-03.
+//
+
+import Testing
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
